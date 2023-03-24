118cba7cc8a0de9a06ec54189a040ace61805d28
lucaruboni <lucaruboni@gmail.com>
2023-03-24 17:47:23 +0100

    [add] markup dinamico con js tramite array di oggetti

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,19 +99,19 @@ const {id, content, media, author, likes ,created} = posts
 
 
 
-const post1 = [posts[0]['id'], posts[0]['media'], posts[0]['author'], posts[0]['likes'], posts[0]['created']]
+const post1 = [posts[0]['id'], posts[0]['content'], posts[0]['media'], posts[0]['author'], posts[0]['likes'], posts[0]['created']]
 
 
-const post2 = [posts[1]['id'], posts[1]['media'], posts[1]['author'], posts[1]['likes'], posts[1]['created']]
+const post2 = [posts[1]['id'], posts[1]['content'], posts[1]['media'], posts[1]['author'], posts[1]['likes'], posts[1]['created']]
 
 
-const post3 = [posts[2]['id'], posts[2]['media'], posts[2]['author'], posts[2]['likes'], posts[2]['created']]
+const post3 = [posts[2]['id'], posts[2]['content'], posts[2]['media'], posts[2]['author'], posts[2]['likes'], posts[2]['created']]
 
 
-const post4 = [posts[3]['id'], posts[3]['media'], posts[3]['author'], posts[3]['likes'], posts[3]['created']]
+const post4 = [posts[3]['id'], posts[3]['content'], posts[3]['media'], posts[3]['author'], posts[3]['likes'], posts[3]['created']]
 
 
-const post5 = [posts[4]['id'], posts[4]['media'], posts[4]['author'], posts[4]['likes'], posts[4]['created']]
+const post5 = [posts[4]['id'], posts[4]['content'], posts[4]['media'], posts[4]['author'], posts[4]['likes'], posts[4]['created']]
 
 
 
@@ -127,6 +127,24 @@ const postArray = [
     post5
 ]
 
+
+const authorArray = [
+    posts[0]['author'],
+
+    posts[1]['author'],
+
+    posts[2]['author'],
+
+    posts[3]['author'],
+
+    posts[4]['author']    
+]
+
+console.log(authorArray, 'array degli autori')
+
+
+
+
 console.log(postArray)
 //stampo i post nella dom(feed)
 
@@ -135,7 +153,14 @@ console.log(postArray)
 const containerElement = document.getElementById('container')
 console.log(containerElement)
 
-//creo una variabile in cui scrivo gia il markup
+//creo una variabile per avere una data casuale
+
+function generateRandomDate() {
+    return new Date(+(new Date()) - Math.floor(Math.random() * 10000000000));
+    }
+    
+    (new generateRandomDate()).toLocaleDateString('en-US')
+
 
 
 
@@ -143,25 +168,38 @@ console.log(containerElement)
                             
 
     //ciclo per stampare i post nella dom
+   
     
-    
+  //ciclo l'array degli autori in modo da avere accesso ai dati all'interno  
+for (let i = 0; i < authorArray.length; i++) {
+        const element = authorArray[i];
+         console.log(element, 'helloooooooooooooooooooooooooo')
+          
+           
+}
+
+
+
+
+//scrivo il contenuto da inserire nella dom 
+
 for (let i = 0; i < postArray.length; i++) {
      const postSrc = postArray[i];
      const postInTheDom = ` <div class="post">
                             <div class="post__header">
                                 <div class="post-meta">                    
                                     <div class="post-meta__icon">
-                                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                                        <img class="profile-pic" src="${authorArray[i]['image']}" alt="Phil Mangione">                    
                                     </div>
                                     <div class="post-meta__data">
-                                        <div class="post-meta__author">Phil Mangione</div>
-                                        <div class="post-meta__time">4 mesi fa</div>
+                                        <div class="post-meta__author">${authorArray[i]['name']}</div>
+                                        <div class="post-meta__time">${generateRandomDate()}</div>
                                     </div>                    
                                 </div>
                             </div>
-                            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                            <div class="post__text">${postArray[i][1]}</div>
                             <div class="post__image">
-                                <img src="https://unsplash.it/600/300?image=171" alt="">
+                                <img src="${postArray[i][2]}" alt="">
                             </div>
                             <div class="post__footer">
                                 <div class="likes js-likes">
@@ -172,7 +210,7 @@ for (let i = 0; i < postArray.length; i++) {
                                         </a>
                                     </div>
                                     <div class="likes__counter">
-                                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                                        Piace a <b id="like-counter-1" class="js-likes-counter">${postArray[i][4]}</b> persone
                                     </div>
                                 </div> 
                             </div>            
